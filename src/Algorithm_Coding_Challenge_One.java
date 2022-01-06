a1746715d653b2ded75ef65df6170a59992d2a6f
aidelojep <aidelojepeter123@gmail.com>
2022-01-06 16:34:40 +0100

    Coding challenge solution

---
+++ src/Algorithm_Coding_Challenge_One.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Algorithm_Coding_Challenge_One {
+    public static void main(String[] args) {
+        System.out.println(solutionMap(new int[]{1, 4, 3, 5}, 7));
+    }
+
+    public static boolean solutionMap(int[] arr, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length - 1; i++) {
+            int remainder = target - arr[i];
+            if (map.containsKey(remainder)){
+                return true;
+            }else {
+                map.put(arr[i], i);
+            }
+        }
+
+        return false;
+    }
+}
